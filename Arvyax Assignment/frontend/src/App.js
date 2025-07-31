ee181ba5b2149b76ccfbffb131f2c95bb1d5da1a
Swapnil Sandeep Gite <162311616+SwapnilGite1311@users.noreply.github.com>
2025-07-31 08:32:51 +0530

    Add files via upload

---
+++ Arvyax Assignment/frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
